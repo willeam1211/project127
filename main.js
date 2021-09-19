8fe2a12701ca4157b3bf018d2c7f2de8792916c4
willeam1211 <81053944+willeam1211@users.noreply.github.com>
2021-09-18 17:23:06 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play() {
+    song.play();
+    song.setVolume(0.5);
+    song.rate(1);
+}
+
+function modelLoaded() {
+    console.log("Posenet is initialized");
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
+}
